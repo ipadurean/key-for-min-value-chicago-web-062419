c4488a708a759694e765ea11756573b7a5cd6958
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-23 15:28:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     nil
   else
   lowest_value = name_hash.first[1]
-  lowest_key = nil
+  lowest_key = name_hash.first[0]
 name_hash.each do |key, value|
   if value < lowest_value
     lowest_value = value
